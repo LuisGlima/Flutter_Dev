863848b851ee308d39fccee75ded4a26c80f2cc8
LuisGlima <defaultretro20@gmail.com>
2024-12-11 23:00:10 -0300

    Estrutura inicial
    pastas para organizar o projet

---
+++ lib/utils/todo_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.deleteFunction,
+  });
+
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
