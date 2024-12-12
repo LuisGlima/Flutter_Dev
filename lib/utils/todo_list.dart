870142c6f2479f9a67403114cff9d7fc934bbbd5
LuisGlima <defaultretro20@gmail.com>
2024-12-11 23:05:22 -0300

    Pastas com dependência

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -14,3 +14,59 @@ class TodoList extends StatelessWidget {
   final bool taskCompleted;
   final Function(bool?)? onChanged;
   final Function(BuildContext)? deleteFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 0,
+      ),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                checkColor: Colors.black,
+                activeColor: Colors.white,
+                side: const BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                  decorationColor: Colors.white,
+                  decorationThickness: 2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
